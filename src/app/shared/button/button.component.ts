a2d0217fbea9595749882cec6019b0ef1f63630f
Carlos Benavides <benavidesdelac.5@teleperformance.com>
2022-11-26 15:01:31 -0500

    chore: delete unused code

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,15 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
   @Input() class!: string;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 }
